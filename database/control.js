6c6b779a64014111530950e40283afa0efefb73e
erbert-gadelha <48739173+erbert-gadelha@users.noreply.github.com>
2023-11-30 22:20:55 -0300

    erro ao conectar com o BD é explicitado

--- database/control.js
+++ database/control.js
@@ -3,8 +3,20 @@ const { Client } = require('pg')
 const connectionString = process.env.DATABASE_URL;
 const client = new Client(connectionString);
 
+class DatabaseError extends Error {
+    constructor(message) {
+        super(message);
+        this.name = "DatabaseError";
+    }
+}
+
 client.connect(function(err) {
-    if (err) console.error(err);
+    if (err) {
+        if(err.code === 'ECONNREFUSED')
+            throw new DatabaseError('Database connection failed: connection refused');
+        else if(err.code === 'ENOTFOUND')
+            throw new DatabaseError(`Database connection failed: host ('${process.env.DATABASE_URL}') not found`);
+    }
     else console.log(`Connected to database at adress "${connectionString}"!`);
 }); 
 
